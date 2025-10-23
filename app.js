9c767e866d061115557805a27f33f044b40ee3f1
bilalcoder-28 <bilalasif2007@gamil.com>
2025-10-23 23:31:14 +0500

    hello

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 let number = 7 
-let secret = prompt("Gusse The secret Number ")
+let secret = +prompt("Gusse The secret Number ")
 if(number == secret){
-    alert("Bingo! Correct Answer")
+    // alert("Bingo! Correct Answer")
+    console.log("Bingo! Correct Answer")
 }
-else if (number == ++secret){
-    alert("Closed Enough to the Correct Answer")
-}else{
-    alert("Wrong")
+else if (number == secret+1 || number == secret-1){
+    // alert("Closed Enough to the Correct Answer")
+    console.log("Closed Enough to the Correct Answer")
+}
+else if (number == secret+2 || number == secret-2){
+    // alert("Closed Enough to the Correct Answer")
+    console.log("Thora or dhur ho")
+}
+else if (number == secret+3 || number == secret-3){
+    // alert("Closed Enough to the Correct Answer")
+    console.log("Both he dhur ho")
+}
+
+else{
+    // alert("Wrong")
+    console.log("Wrong")
 }
